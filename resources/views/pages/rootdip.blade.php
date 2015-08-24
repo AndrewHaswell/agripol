b5a5eed0ece11d783bb6f9bc1c0d150137c6daf8
Andrew Haswell <andrew.haswell@visualsoft.co.uk>
2015-08-24 16:44:31 +0100

    More pages

---
+++ resources/views/pages/rootdip.blade.php
@@ -0,0 +1,114 @@
+@extends('homepage')
+
+@section('content')
+
+    <div class="medium-banner">
+        <h1>Broadleaf Root Dip</h1>
+    </div>
+
+    <div class="container">
+
+        <div class="row">
+
+            <p>Broadleaf Root Dip is beneficial for all sizes of softwood and hardwood trees and on all species of
+                bushes and shrubs. It is also ideal for bare-root transplanting of vegetable crops and international
+                transporting of bare-root plant material.</p>
+
+            <h3>USE IN CONJUNCTION WITH BROADLEAF P4 FOR LONG TERM BENEFITS</h3>
+
+            <p>It should be noted that bare-root dipping is intended to act only as an anti-desiccant to extend
+                out-of-soil life and counteract transplant shock. It is not designed to replace the incorporation of
+                Broadleaf P4 into tree-planting pockets, which will still be needed to supply roots with adequate
+                moisture to support a sizeable leaf canopy over an extended period of time after transplanting.</p>
+
+            <p>Bare-root treatment with BROADLEAF ROOT DIP and planting-pocket incorporation of Broadleaf P4 can be used
+                together in a complementary manner to great short and long term benefit.</p>
+
+            <h3>How Much Broadleaf Root Dip will be needed to treat a given quantity of bare-root stock?</h3>
+
+            <p>The amount of gel that can be loaded onto the bare roots will depend on the type of root system (e.g.
+                fibrous & bushy or spindly) and the consistency of the gel.</p>
+
+            <p>The following table shows the average quantity of bare-root stock that can be dipped with 1 Kg of powder,
+                hydrated in average quality water. Please use for guidance only.</p>
+
+
+            <table>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>Grade (cm)</td>
+                    <td>Quantity</td>
+                </tr>
+                <tr>
+                    <td>Transplants</td>
+                    <td>45-60</td>
+                    <td>1300</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>60-90</td>
+                    <td>1100</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>90-180</td>
+                    <td>900</td>
+                </tr>
+                <tr>
+                    <td>Feathered Whips</td>
+                    <td>120-180</td>
+                    <td>600</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>180-240</td>
+                    <td>500</td>
+                </tr>
+                <tr>
+                    <td>Trees</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>Light Standard</td>
+                    <td>6-8</td>
+                    <td>400</td>
+                </tr>
+                <tr>
+                    <td>Standard</td>
+                    <td>8-10</td>
+                    <td>300</td>
+                </tr>
+                <tr>
+                    <td>Selected Standard</td>
+                    <td>10-12</td>
+                    <td>150</td>
+                </tr>
+                <tr>
+                    <td>Heavy Standard</td>
+                    <td>12-14</td>
+                    <td>80</td>
+                </tr>
+                <tr>
+                    <td>Extra-heavy Standard</td>
+                    <td>14-16</td>
+                    <td>60</td>
+                </tr>
+                <tr>
+                    <td>Extra-heavy Standard</td>
+                    <td>16-18</td>
+                    <td>20</td>
+                </tr>
+            </table>
+
+            <h3>TRANSPORTING TREATED STOCK</h3>
+
+            <p>If dipped plant material has to be transported any significant distance to the planting site, cover the
+                roots with a suitable carrier or container, such as a plastic wrap, to retain the gel around the
+                roots.</p>
+
+        </div>
+
+    </div>
+
+@endsection
